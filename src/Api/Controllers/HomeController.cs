8aac2629c368d908fa59bd0af25f4c02bf829746
Frederik Gadegaard <feg@novicell.dk>
2023-05-05 14:17:22 +0200

    Added home controller

---
+++ src/Api/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Users.Models;
+
+namespace WebApi.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+            
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            return Ok(Task.FromResult("Hello from Healthybois").Result);
+        }
+    }
+}
