efa30dcd8d932396095e3c768aa4d6e2fc1441f0
Oscar Schou <oscarschou@MacBook-Pro.local>
2023-05-19 14:19:26 +0200

    nav added for non admin

--- src/Frontend/src/_components/Nav.jsx
+++ src/Frontend/src/_components/Nav.jsx
@@ -27,14 +27,149 @@ function Nav() {
 
     if (auth.isAdmin === false) {
         return (
-            <nav className="navbar navbar-expand navbar-dark bg-dark px-3">
-                <div className="navbar-nav">
-                    <NavLink to="/" className="nav-item nav-link">Home</NavLink>
-                    {/* <NavLink to="/users" className="nav-item nav-link">Users</NavLink> */}
-                    <NavLink to="/events" className="nav-item nav-link">Events</NavLink>
-                    <button onClick={logout} className="btn btn-link nav-item nav-link">Logout</button>
-                </div>
-            </nav>
+            <Disclosure as="nav" className="bg-gray-800">
+            {({ open, close }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <NavLink to="/" className=""><img
+                                        className="block h-8 w-auto lg:hidden"
+                                        src="/logo.png"
+                                        alt="Healthybois"
+                                    /></NavLink>
+                                    <NavLink to="/" className="">     <img
+                                        className="hidden h-8 w-auto lg:block"
+                                        src="/logo.png"
+                                        alt="Healthybois"
+                                    /></NavLink>
+
+                                </div>
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        <NavLink to="/" className="text-white hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium">Home</NavLink>
+                                        <NavLink to="/users" className="text-white hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium">Users</NavLink>
+                                        <NavLink to="/events" className="text-white hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium">Events</NavLink>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                                <button
+                                    type="button"
+                                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                >
+                                    <span className="sr-only">View notifications</span>
+                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                </button>
+
+                                {/* Profile dropdown */}
+                                <Menu as="div" className="relative ml-3">
+                                    <div>
+                                        <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                            <span className="sr-only">Open user menu</span>
+                                            <img
+                                                className="h-8 w-8 rounded-full"
+                                                src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                                                alt="rando Dude"
+                                            />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="www.test.dk"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Your Profile
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="www.test.dk"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Settings
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <button onClick={logout} className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>Logout</button>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+
+                            <Disclosure.Button className="">
+                            <NavLink
+                  to="/"
+                  onClick={close} 
+                  className={`block rounded-md px-3 py-2 text-base font-medium ${
+                    pathname === '/' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                  }`}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/users"
+                  onClick={close} 
+                  className={`block rounded-md px-3 py-2 text-base font-medium ${
+                    pathname === '/users' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                  }`}
+                >
+                  Users
+                </NavLink>
+                <NavLink
+                  to="/events"
+                  onClick={close} 
+                  className={`block rounded-md px-3 py-2 text-base font-medium ${
+                    pathname === '/events' ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                  }`}
+                                >
+                                    Events
+                                </NavLink>
+                            </Disclosure.Button>
+
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
         );
     }
 
