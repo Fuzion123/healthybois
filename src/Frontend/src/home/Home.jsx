79a1a6bc1d05b4abe39b2c9d992418c8d79fcc6d
Oscar Schou <oscarschou@macbook-pro.home>
2023-05-21 11:40:00 +0200

    abit more homepage

--- src/Frontend/src/home/Home.jsx
+++ src/Frontend/src/home/Home.jsx
@@ -19,42 +19,42 @@ function Home() {
             {/* Sections */}
             <div className="container mx-auto px-4 py-8">
               <section className="mb-8">
-                <h2 className="text-xl font-bold mb-4">Section 1</h2>
+                <h2 className="text-xl font-bold mb-4">Upcoming Events</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 1</h3>
+                  <div className="p-8">
+                    <h3 className="text-md font-bold mb-2">Box 1</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 2</h3>
+                  <div className="bg-green-50 p-8 shadow-md rounded-lg">
+                    <h3 className="text-md font-bold mb-2">Data box</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
               </section>
       
               <section className="mb-8">
-                <h2 className="text-xl font-bold mb-4">Section 2</h2>
+                <h2 className="text-xl font-bold mb-4">Highlighted Profile</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 1</h3>
+                  <div className="p-8">
+                    <h3 className="text-md font-bold mb-2">Box 1</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 2</h3>
+                  <div className="bg-yellow-50 p-8 shadow-md rounded-lg">
+                    <h3 className="text-md font-bold mb-2">Data box</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
               </section>
       
               <section>
-                <h2 className="text-xl font-bold mb-4">Section 3</h2>
+                <h2 className="text-xl font-bold mb-4">Latest post</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 1</h3>
+                  <div className="p-8">
+                    <h3 className="text-md font-bold mb-2">Box 1</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
-                  <div className="bg-white p-8 shadow-md rounded-lg">
-                    <h3 className="text-xl font-bold mb-2">Box 2</h3>
+                  <div className="bg-cyan-50 p-8 shadow-md rounded-lg">
+                    <h3 className="text-md font-bold mb-2">Data box</h3>
                     <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   </div>
                 </div>
@@ -74,42 +74,42 @@ function Home() {
         {/* Sections */}
         <div className="container mx-auto px-4 py-8">
           <section className="mb-8">
-            <h2 className="text-xl font-bold mb-4">Section 1</h2>
+            <h2 className="text-xl font-bold mb-4">Upcoming Events</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 1</h3>
+              <div className="p-8">
+                <h3 className="text-md font-bold mb-2">Box 1</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 2</h3>
+              <div className="bg-green-50 p-8 shadow-md rounded-lg">
+                <h3 className="text-md font-bold mb-2">Data box</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
           </section>
   
           <section className="mb-8">
-            <h2 className="text-xl font-bold mb-4">Section 2</h2>
+            <h2 className="text-xl font-bold mb-4">Highlighted Profile</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 1</h3>
+              <div className="p-8">
+                <h3 className="text-md font-bold mb-2">Box 1</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 2</h3>
+              <div className="bg-yellow-50 p-8 shadow-md rounded-lg">
+                <h3 className="text-md font-bold mb-2">Data box</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
           </section>
   
           <section>
-            <h2 className="text-xl font-bold mb-4">Section 3</h2>
+            <h2 className="text-xl font-bold mb-4">Latest post</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 1</h3>
+              <div className="p-8">
+                <h3 className="text-md font-bold mb-2">Box 1</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
-              <div className="bg-white p-8 shadow-md rounded-lg">
-                <h3 className="text-xl font-bold mb-2">Box 2</h3>
+              <div className="bg-cyan-50 p-8 shadow-md rounded-lg">
+                <h3 className="text-md font-bold mb-2">Data box</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
