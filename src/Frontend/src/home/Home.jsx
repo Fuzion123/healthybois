7296df90aeff2c76d5873686c2c98f9d0422cc47
Frederik Gadegaard <feg@novicell.dk>
2023-05-28 00:44:28 +0200

    added event data to home

--- src/Frontend/src/home/Home.jsx
+++ src/Frontend/src/home/Home.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import React from 'react';
-
-
+import { eventapi } from '../_api'
+import {useQuery } from 'react-query';
 
 export { Home };
 
@@ -9,62 +9,14 @@ function Home() {
 
   const auth = useSelector(x => x.auth.value);
 
+  // query
+  const {data, error, isLoading} = useQuery('getAllEvents', () => {
+    return eventapi.getAll();
+  });
 
+  if (error) return <div>Request Failed</div>;
 
-  if (auth?.isAdmin === true) {
-    return (
-      <div className="min-h-screen">
-        {/* Header */}
-        <header className="text-center py-4 px-8">
-          <h1 className="text-2xl font-bold">Welcome {auth?.firstName}</h1>
-        </header>
-
-        {/* Sections */}
-        <div className="container mx-auto px-4 py-8">
-          <section className="mb-8">
-            <h2 className="text-xl font-bold mb-4">Ongoing Cup</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="p-8">
-                <p className="text-gray-600">You are currently a competitor in this cup: </p>
-              </div>
-              <div className="bg-green-50 p-8 shadow-md rounded-lg">
-                <h3 className="text-md font-bold mb-2">Data box</h3>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </div>
-          </section>
-
-          <section className="mb-8">
-            <h2 className="text-xl font-bold mb-4">Highlighted Profile</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="p-8">
-                <h3 className="text-md font-bold mb-2">Box 1</h3>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-              <div className="bg-yellow-50 p-8 shadow-md rounded-lg">
-                <h3 className="text-md font-bold mb-2">Data box</h3>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <h2 className="text-xl font-bold mb-4">Latest post</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              <div className="p-8">
-                <h3 className="text-md font-bold mb-2">Box 1</h3>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-              <div className="bg-cyan-50 p-8 shadow-md rounded-lg">
-                <h3 className="text-md font-bold mb-2">Data box</h3>
-                <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    );
-  }
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="min-h-screen">
@@ -74,6 +26,18 @@ function Home() {
       </header>
 
       {/* Sections */}
+
+        <h1 className='text-3xl'>Events</h1>
+        <div>
+          {data.map((p, i) => (
+            <div key={i}>
+              <p>{p.title}</p>
+              <p>{p.eventOwner.email}</p>
+              <p>{p.eventOwner.firstName}</p>
+            </div>
+          ))}
+        </div>
+
       <div className="container mx-auto px-4 py-8">
         <section className="mb-8">
           <h2 className="text-xl font-bold mb-4">Upcoming Events</h2>
