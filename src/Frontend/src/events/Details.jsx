8bdff969c4a9a2f46c821125aa8f32244536f7df
Oscar Schou <oscarschou@MacBook-Pro.local>
2023-05-27 19:35:16 +0200

    details view progress

--- src/Frontend/src/events/Details.jsx
+++ src/Frontend/src/events/Details.jsx
@@ -3,18 +3,23 @@ import { confirmAlert } from 'react-confirm-alert';
 import {useQuery, useMutation } from 'react-query';
 import { eventapi } from '_api';
 import { history, date } from '_helpers';
-import  BackButton  from '../_components/BackButton';
 
 
 export default  EventDetails;
 
 function EventDetails() {
   const { id } = useParams();
+ 
 
-	// query
-	const {data, error, isLoading} = useQuery('getById', () => {
+  // query
+  const {data, error, isLoading} = useQuery('getById', () => {
     return eventapi.getById(id);
-  });
+  },{
+    onSuccess: () => {
+      console.log(data)
+    }
+  }
+  );
 
   const mutation = useMutation(async (id) => {
     await eventapi.deleteById(id)
@@ -24,9 +29,9 @@ function EventDetails() {
     }
   });
 
-	if (error) return <div>Request Failed</div>;
+  if (error) return <div>Request Failed</div>;
 
-	if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
 const submit = (event) => {
   confirmAlert({
@@ -58,128 +63,78 @@ const submit = (event) => {
 
 return (
   <div>
-    {<BackButton />}
     {(data) && (
-      <div>
-        <h1>{data.title}</h1>
-        <div className="card">
-          <div className="card-body">
-            <img src='https://play-lh.googleusercontent.com/N6yBgv77P8b3T2gZu4ARO8kBjZ0nPMt1pKKip1ox4b-jw8lvqfC-pLcoBWVJwSsfnQ=s256-rw' alt='stock'></img>
-            <section className="mt-3">
-              <h4>Details:</h4>
-              <p>Arranged by: </p>
-              <p>Starts at: {date.formatDate(data.startsAt)}</p>
-              <p>Ends at: {date.formatDate(data.endsAt)}</p>
-            </section>
-          </div>
-        </div>
-        <div className="card mt-3">
-          <div className="card-body">
-            <h4>Participants:</h4>
-            <ul className="list-group">
-              {data.participants.map((p, index) => (
-                <li
-                  key={p.userId}
-                  className={`list-group-item ${index % 2 === 0 ? 'bg-light' : ''}`}
-                >
-                  <p>UserId: {p.userId}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+    <div className="bg-white py-24 sm:py-32">
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <img src= {data.eventPictureUrl} alt='stock'></img>
+      <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          <article key="event details"className="flex max-w-xl flex-col items-start justify-between">
+            <div className="flex items-center gap-x-4 text-xs">
+              <time dateTime={data.startsAt} className="text-gray-500">
+              {date.formatDate(data.startsAt)}
+              </time>
+              <p className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                Endurance
+              </p>
+            </div>
+            <div className="group relative">
+              <h1 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <span className="absolute inset-0" />
+                {data.title}
+              </h1>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{data.description}</p>
+            </div>
+            <div className="relative mt-8 flex items-center gap-x-4">
+              <img src={data.eventOwner.profilePictureUrl} alt="Profile Pic" className="h-10 w-10 rounded-full bg-gray-50" />
+              <div className="text-sm leading-6">
+                <p className="font-semibold text-gray-900">
+                    <span className="absolute inset-0" />
+                    {data.eventOwner.firstName}
+
+                </p>
+                <p className="text-gray-600">host</p>
+              </div>
+            </div>
+          </article>
+      </div>
+
+        
         <div className="d-flex justify-content-between align-items-center mt-3">
         <button onClick={() => submit(data)} className="btn btn-sm btn-danger mx-2" disabled={data.isDeleting}>
           <span>Delete</span>
         </button>
-        <button className="btn btn-sm btn-primary mx-2" onClick={() => {
-const eventName = data.title;
-const eventLink = window.location.href;
-const recipient = "healthyboi@example.com";
-const subject = "Invitation to join my event";
-const body = `
-  <p>Hey Healthyboi,</p>
-  <p>You are hereby invited to join my event: <strong>${eventName}</strong>.</p>
-  <p>Are you up for the challenge?</p>
-  <p><a href="${eventLink}">Join here</a></p>
-  <p>Best regards,</p>
-  <p>Your Host</p>
-`;
-
-window.location.href = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-}}>Invite</button>
-              <button className="btn btn-sm btn-success mx-2">Sign up</button>
-      </div>
-      </div>
-    )}
-    {data?.loading && (
-      <div className="text-center">
-        <span className="spinner-border spinner-border-lg align-center"></span>
-      </div>
-    )}
-  </div>
-);
+        </div>
+        </div>
+        </div>
+      
+      
+)
+              }
+              </div>
 
 
-}
+//  <button className="btn btn-sm btn-primary mx-2" onClick={() => {
+// const eventName = data.title;
+// const eventLink = window.location.href;
+// const recipient = "healthyboi@example.com";
+// const subject = "Invitation to join my event";
+// const body = `
+//   <p>Hey Healthyboi,</p>
+//   <p>You are hereby invited to join my event: <strong>${eventName}</strong>.</p>
+//   <p>Are you up for the challenge?</p>
+//   <p><a href="${eventLink}">Join here</a></p>
+//   <p>Best regards,</p>
+//   <p>Your Host</p>
+// `;
 
-
-<style scoped>
-{`
-
-
-        #react-confirm-alert {
-         display: flex;
-         justify-content: center;
-         text-align: center !important; 
-         margin: 5px;
-        }
-
-        .btn btn-sm btn-danger {
-          z-index: 999;
-        }
-
-        .react-confirm-alert-button-group {
-         display: flex;
-         justify-content: space-evenly;
-         margin: 8px;
-        }
-
-        .react-confirm-alert-body {
-         text-align: center !important; 
-         border-style: groove;
-         border-color: rgb(43, 42, 41);
-         border-radius: 15px;
-         padding: 18px;
-         width: 300px;
-        }
-
-        .confirm-title {
-         font-weight: bold;
-         color: #212529;
-         display: flex;
-         justify-content: center;
-         margin: 10px;
-        }
-
-        .confirm-message  {
-         color: #212529;
-         display: flex;
-         justify-content: center;
-         text-align: center;
-        }
-
-        a {
-          text-decoration: none;
-          color: #212529;
-        }
-
-        @media only screen and (min-width: 800px) {
-          .react-confirm-alert-body{
-            width: 400px;
-          }
-        }
-        
-      `}
-
-</style>
+// window.location.href = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+// }}>Invite</button>
+//               <button className="btn btn-sm btn-success mx-2">Sign up</button>
+//       </div>
+//       </div>
+//     )}
+//     {data?.loading && (
+//       <div className="text-center">
+//         <span className="spinner-border spinner-border-lg align-center"></span>
+//       </div>
+  )}
