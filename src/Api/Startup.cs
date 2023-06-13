31fa6926f98512ae8edc4ea8c6bc5b5025b0f87e
Frederik Gadegaard <feg@novicell.dk>
2023-06-13 21:48:40 +0200

    fixed cors when using local FE to hit production BE API

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -55,20 +55,27 @@ namespace WebApi
             {
                 options.AddDefaultPolicy(builder =>
                 {
-                    if (webHostEnvironment.IsProduction())
-                    {
-                        builder.WithOrigins("https://happy-field-0e3c42103.3.azurestaticapps.net")
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .AllowCredentials();
-                    }
-                    else
-                    {
-                        builder.WithOrigins("http://localhost:3000")
-                            .AllowAnyHeader()
-                            .AllowAnyMethod()
-                            .AllowCredentials();
-                    }
+                    builder
+                        .WithOrigins("https://happy-field-0e3c42103.3.azurestaticapps.net")
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+
+                    //if (webHostEnvironment.IsProduction())
+                    //{
+                    //    builder.WithOrigins("https://happy-field-0e3c42103.3.azurestaticapps.net")
+                    //        .AllowAnyHeader()
+                    //        .AllowAnyMethod()
+                    //        .AllowCredentials();
+                    //}
+                    //else
+                    //{
+                    //    builder.WithOrigins("http://localhost:3000")
+                    //        .AllowAnyHeader()
+                    //        .AllowAnyMethod()
+                    //        .AllowCredentials();
+                    //}
                 });
             });
 
