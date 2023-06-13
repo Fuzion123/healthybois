1bd6490376a2e61c3662698ac4bac92e5fbf7bbc
Oscar Schou <oscarschou@MacBook-Pro.local>
2023-06-14 00:52:58 +0200

    more content to homepage and small adjust in message box for mobile style

--- src/Frontend/src/_components/Messages.jsx
+++ src/Frontend/src/_components/Messages.jsx
@@ -33,7 +33,7 @@ function Messages(props){
   return (
     <div className="container mx-auto px-4 py-8">
         <section className="mb-8">
-        <h2 className="text-xl font-bold mb-4">Incoming messages</h2>
+        <h2 className="text-md font-bold mb-2">Message board</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-h-screen">
             <div className="p-8 overflow-hidden">
               { props.messages.map((m, i) => (
@@ -52,17 +52,17 @@ function Messages(props){
               onChange={handleInputChange}
               onKeyUp={onKeyUp}
               type="text"
-              className="w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+              className="w-28 border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
               value={message}
             />
             <button
               onClick={sendMessage}
-              className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0 ml-2"
+              className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-2 py-1 flex-shrink-0 ml-2"
             >
-              <span>Send</span>
+              <span className="text-xs">Send</span>
               <span className="ml-2">
                 <svg
-                  className="w-4 h-4 transform rotate-45 -mt-px"
+                  className="w-2 h-3 transform rotate-45 -mt-px"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
--- src/Frontend/src/home/Home.jsx
+++ src/Frontend/src/home/Home.jsx
@@ -1,12 +1,24 @@
-import { useSelector } from 'react-redux';
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { eventapi } from '../_api'
 import {useQuery } from 'react-query';
 import { Messages } from '_components';
+import { eventsActions } from '_store';
 
 export { Home };
 
 function Home(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleCardClick(event) {
+    navigate(`/events/${event.id}`);
+  }
+
+  useEffect(() => {
+      dispatch(eventsActions.getReferenceAll());
+  }, []);
 
   const auth = useSelector(x => x.auth.value);
 
@@ -27,31 +39,28 @@ function Home(props) {
       </header>
 
       {/* Sections */}
-        <Messages connection={props.connection} messages = { props.messages }></Messages>
-
-
-        <h1 className='text-3xl'>Events</h1>
-        <div>
-          {data.map((p, i) => (
-            <div key={i}>
-              <p>{p.title}</p>
-              <p>{p.eventOwner.email}</p>
-              <p>{p.eventOwner.firstName}</p>
-            </div>
-          ))}
-        </div>
 
       <div className="container mx-auto px-4 py-8">
         <section className="mb-8">
           <h2 className="text-xl font-bold mb-4">Upcoming Events</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="p-8">
-              <h3 className="text-md font-bold mb-2">Box 1</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text-md font-bold mb-2">Get ready to compete</h3>
+              <p className="text-gray-600">Don't miss out on our upcoming events! Join an event for an unforgettable experience filled with excitement, inspiration, and community. Sign up and claim your spot among the champions!</p>
             </div>
             <div className="bg-green-50 p-8 shadow-md rounded-lg">
-              <h3 className="text-md font-bold mb-2">Data box</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text-md font-bold mb-2">Next 3 events</h3>
+              <p className="text-gray-600">
+                {data
+                  .sort((a, b) => a.startsAt - b.startsAt)
+                  .slice(0, 3)
+                  .map((p, i) => (
+                    <div key={i} onClick={() => handleCardClick(p)}>
+                    <img className="object-cover w-50 h-50 md:h-20 rounded-t-lg" src={p.eventPictureUrl} alt="stock" />
+                    <p>{p.title}</p>
+                    <p>{p.eventOwner.firstName}</p>
+                  </div>
+          ))}</p>
             </div>
           </div>
         </section>
@@ -60,12 +69,12 @@ function Home(props) {
           <h2 className="text-xl font-bold mb-4">Highlighted Profile</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="p-8">
-              <h3 className="text-md font-bold mb-2">Box 1</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text-md font-bold mb-2">Check this guy out</h3>
+              <p className="text-gray-600">With exceptional skills and unmatched dedication, is a force to be reckoned with on in the tourney. Don't miss the opportunity to witness their extraordinary performances or beat them in the upcoming events</p>
             </div>
             <div className="bg-yellow-50 p-8 shadow-md rounded-lg">
-              <h3 className="text-md font-bold mb-2">Data box</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text-md font-bold mb-2">Player in focus</h3>
+              <p className="text-gray-600"></p>
             </div>
           </div>
         </section>
@@ -74,12 +83,11 @@ function Home(props) {
           <h2 className="text-xl font-bold mb-4">Latest post</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             <div className="p-8">
-              <h3 className="text-md font-bold mb-2">Box 1</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text-md font-bold mb-2">Connect with others</h3>
+              <p className="text-gray-600">Join the vibrant message board community and connect with fellow competitors. Share strategies, exchange tips, and engage in meaningful discussions to enhance your competitive journey.</p>
             </div>
             <div className="bg-cyan-50 p-8 shadow-md rounded-lg">
-              <h3 className="text-md font-bold mb-2">Data box</h3>
-              <p className="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p className="text-gray-600"><Messages connection={props.connection} messages = { props.messages }></Messages></p>
             </div>
           </div>
         </section>
