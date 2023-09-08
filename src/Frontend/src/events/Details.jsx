a722bba1ebd98c9af94a96810d69081d00faed10
Frederik Gadegaard <feg@novicell.dk>
2023-09-08 16:30:58 +0200

    updated some styling on result page

--- src/Frontend/src/events/Details.jsx
+++ src/Frontend/src/events/Details.jsx
@@ -53,7 +53,14 @@ return (
             <button onClick={() => history.navigate(`/events/${id}/addActivity`)} className="mt-3 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Add Activity</button>
         </div>
       </div>
-      <button onClick={() => history.navigate(`/events`)} className="mt bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded">Back</button>
+      <button onClick={() => history.navigate(`/events`)} className="flex flex-row mt bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded">
+      <div class="flex flex-row align-middle">
+        <svg class="w-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
+        </svg>
+        <p class="">back</p>
+      </div>
+      </button>
         </> 
      
 )
--- src/Frontend/src/events/activities/ActivityResult.jsx
+++ src/Frontend/src/events/activities/ActivityResult.jsx
@@ -82,24 +82,26 @@ function ActivityResult(props) {
 
   return (
       
-    <div className="">
-      <label htmlFor="score" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">score</label>
-      <div className="flex-row justify-items-center">
-            <div className="flex-shrink-0 w-16 h-16 bg-gray-200 rounded-full overflow-hidden">
-              <img
-                  src={participant.profilePictureUrl}
-                  alt={participant.firstName}
-                  className="object-cover w-full h-full"
-              />
+    <div>
+      <label htmlFor="score" className="mb-2 text-sm font-medium sr-only dark:text-white">score</label>
+      <div className="flex flex-row justify-between items-center bg-white border rounded-lg">
+            <div className="m-3">
+                <div className="justify-center flex-shrink-0 w-16 h-16 rounded-full overflow-hidden">
+                  <img
+                      src={participant.profilePictureUrl}
+                      alt={participant.firstName}
+                      className="object-cover w-full h-full"/>
+                </div>
+                <p className="justify-center">Frederik</p>
             </div>
           
-            <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-              <button disabled={isProcessing} onClick={() => decrease()} className="bg-gray-300 text-gray-600 h-full w-20 rounded-r cursor-pointer">
-                  <span className="m-auto text-2xl font-thin">−</span>
+            <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent m-4">
+              <button disabled={isProcessing} onClick={() => decrease()} className="bg-blue-500  text-white font-bold py-2 px-4">
+                  <span className="m-auto font-thin">−</span>
               </button>
-              <input disabled={isProcessing} type="number" onChange={(e) => scoreChanged(e.target.value)} value={score} className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number"></input>
-              <button disabled={isProcessing} onClick={() => increase()} className="bg-gray-300 text-gray-600 h-full w-20 rounded-r cursor-pointer">
-                <span className="m-auto text-2xl font-thin">+</span>
+              <input disabled={true} type="number" onChange={(e) => scoreChanged(e.target.value)} value={score} className="border-blue-500 text-center w-full bg-blue-500 font-semibold text-md hover:text-white focus:text-white md:text-basecursor-default flex items-center text-white" name="custom-input-number"></input>
+              <button disabled={isProcessing} onClick={() => increase()} className="bg-blue-500  text-white font-bold py-2 px-4 ">
+                <span className="m-auto font-thin">+</span>
               </button>
           </div>
 
