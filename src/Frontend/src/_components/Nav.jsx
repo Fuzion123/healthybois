f900d817e3135e8c766c325dc3cab4afed4a4681
Frederik Gadegaard <feg@novicell.dk>
2023-10-13 16:34:48 +0200

    made your profile availailble for normal users

--- src/Frontend/src/_components/Nav.jsx
+++ src/Frontend/src/_components/Nav.jsx
@@ -113,7 +113,7 @@ function Nav() {
                         <Menu.Item>
                           {({ active }) => (
                             <a
-                              href="www.test.dk"
+                              href="/profile/YourProfile"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
